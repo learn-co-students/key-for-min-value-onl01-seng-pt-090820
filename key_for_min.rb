417a455ddfeb8c59955b17d9c0504c27e0794922
Ana Betancourt <anaa.betancourt13@gmail.com>
2020-09-19 05:37:51 +0000

    Done.

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,9 +5,9 @@ def key_for_min_value(name_hash)
   lowest_value = nil
   key_value = nil
   name_hash.collect do |name, number|
-    if lowest value = nil || number < lowest_value
-      number = lowest_value
-      name = key_value
+    if lowest_value == nil || number < lowest_value
+      lowest_value = number
+      key_value = name
    end
   end
   key_value
